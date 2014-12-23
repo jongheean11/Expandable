3171d7ecc3f6049cfa7a575537ba191ebc13368e
JamesKim <jongheean11@outlook.com>
2014-12-24 01:21:45 +0900

    전 버그 수정3

---
+++ Expandable/E_DesktopSwitcher.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "E_DesktopSwitcher.h"
+
+void E_DesktopSwitcher::drawWindowSwitcher()
+{
+
+}
+
+void E_DesktopSwitcher::drawPreview()
+{
+
+}
+
+void E_DesktopSwitcher::onMouseClick()
+{
+
+}
+
+void E_DesktopSwitcher::switchDesktop(int direction)
+{
+
+}
+
+void E_DesktopSwitcher::drawOverview(E_Desktop* targetDesktop, E_Window* targetWindow)
+{
+
+}
+
+void E_DesktopSwitcher::updateComponent(E_Desktop* targetDesktop, E_Window* targetWindow)
+{
+
+}
---
+++ Expandable/E_DesktopSwitcher.h
@@ -0,0 +1,15 @@
+#include <afxwin.h>
+#include "E_Desktop.h"
+
+class E_DesktopSwitcher
+{
+private:
+	
+public:
+	void drawWindowSwitcher();
+	void drawPreview();
+	void onMouseClick();
+	void switchDesktop(int direction);
+	void drawOverview(E_Desktop* targetDesktop, E_Window* targetWindow);
+	void updateComponent(E_Desktop* targetDesktop, E_Window* targetWindow);
+};
