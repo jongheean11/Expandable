17357f873fb2c9510be5012766c71c230cbae757
JamesKim <jongheean11@outlook.com>
2014-12-24 03:28:23 +0900

    test123

---
+++ Expandable/E_Global.cpp
@@ -0,0 +1,71 @@
+#include "stdafx.h"
+#include "E_Global.h"
+
+E_Global::E_Global()
+{
+
+}
+E_Global::~E_Global()
+{
+
+}
+
+void E_Global::setresolutionWidth(int paramWidth)
+{
+	resolutionWidth = paramWidth;
+}
+
+double E_Global::getresolutionWidth()
+{
+	return resolutionWidth;
+}
+
+void E_Global::E_Global::setresolutionHeight(int paramHeight)
+{
+	resolutionHeight = paramHeight;
+}
+
+double E_Global::getresolutionHeight()
+{
+	return resolutionHeight;
+}
+
+void E_Global:: setVirtualWidth(int paramWidth)
+{
+	virtualWidth = paramWidth;
+}
+
+double E_Global::getVirtualWidth()
+{
+	return virtualWidth;
+}
+
+void E_Global::E_Global::setVirtualHeight(int paramHeight)
+{
+	virtualHeight = paramHeight;
+}
+
+double E_Global::getVirtualHeight()
+{
+	return virtualHeight;
+}
+
+void E_Global::E_Global::setSelectedDesktopIndex(int paramIndex)
+{
+	selectedDesktopIndex = paramIndex;
+}
+
+int E_Global::getSelectedDesktopIndex()
+{
+	return selectedDesktopIndex;
+}
+
+void E_Global::E_Global::startUpdateSelectedDesktop()
+{
+
+}
+
+void E_Global::E_Global::stopUpdateSelectedDesktop()
+{
+
+}
