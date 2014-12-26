cfe75c7fdd2ec59ac1a11d804bd76e565082493c
je hyung <imalwaysgood@naver.com>
2014-12-26 20:01:27 +0900

    map

--- Expandable/E_Map.cpp
+++ Expandable/E_Map.cpp
@@ -1,1 +1,30 @@
-sd
+#include"E_Map.h"
+void E_Map::drawMap()
+{
+
+}
+
+void E_Map::onMouseLButtonDown()
+{
+
+}
+
+void E_Map::onMouseMove()
+{
+
+}
+
+void E_Map::OnMouseLButtonUp()
+{
+
+}
+
+void E_Map::startTimer()
+{
+
+}
+
+void E_Map::stopTimer()
+{
+
+}
--- Expandable/E_Map.h
+++ Expandable/E_Map.h
@@ -1,1 +1,15 @@
-sd
+#include "E_Desktop.h"
+#include "E_Window.h"
+class E_Map
+{
+private:
+	int visibleTime;
+	E_Window* draggingWindow;
+public:
+	void drawMap();
+	void onMouseLButtonDown();
+	void onMouseMove();
+	void OnMouseLButtonUp();
+	void startTimer();
+	void stopTimer();
+};
