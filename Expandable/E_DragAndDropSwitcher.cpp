434a5aa9180b884d94bab63acfdb00c1a210d7aa
JamesKim <jongheean11@outlook.com>
2015-01-15 17:25:38 +0900

    DesktopSwitcher 잔버그 수정 (AeroPeek Boolean)

--- Expandable/E_DragAndDropSwitcher.cpp
+++ Expandable/E_DragAndDropSwitcher.cpp
@@ -2,6 +2,7 @@
 #include "E_DragAndDropSwitcher.h"
 #include "E_Global.h"
 #include "E_EnvironmentManager.h"
+#include "E_AeroPeekController.h"
 
 void E_DragAndDropSwitcher::updateSelectedDesktop()
 {
@@ -9,8 +10,46 @@ void E_DragAndDropSwitcher::updateSelectedDesktop()
 	TRACE_WIN32A("[E_DragAndDropSwitcher::updateSelectedDesktop()]");
 }
 
+void drawCurrentDesktop()
+{
+}
+
+void drawSwitchDesktop()
+{
+}
+
+void drawDragAndDropSwitcher()
+{
+	E_Global* e_global = E_Global::getSingleton();
+	E_EnvironmentManager* enManager = E_EnvironmentManager::getSingleton();
+	E_DragAndDropSwitcher* drSwitcher = E_DragAndDropSwitcher::getSingleton();
+	E_AeroPeekController* aeController = E_AeroPeekController::getSingleton();
+
+	PAINTSTRUCT ps;
+	HDC hdc;
+	HTHUMBNAIL pushThumbnail;
+
+	drSwitcher->hShellWnd = GetShellWindow(); // 바탕화면 hwnd
+	GetWindowRect(drSwitcher->hShellWnd, &(drSwitcher->sizeRect_background)); // 바탕화면 크기 얻기
+	drSwitcher->hTaskbarWnd = FindWindowW(_T("Shell_TrayWnd"), NULL); // 작업표시줄 hwnd
+	GetWindowRect(drSwitcher->hTaskbarWnd, &(drSwitcher->sizeRect_taskbar)); // 작업표시줄 크기 얻기
+
+	drawCurrentDesktop();
+
+	drawSwitchDesktop();
+
+	hdc = BeginPaint(*drSwitcher, &ps);
+	EndPaint(*drSwitcher, &ps);
+}
+
+
 E_DragAndDropSwitcher::E_DragAndDropSwitcher()
 {
+	ison = false;
+	cursor_left = false;
+	cursor_right = false;
+	cursor_top = false;
+	cursor_bottom = false;
 }
 
 E_DragAndDropSwitcher* E_DragAndDropSwitcher::singleton = NULL;
@@ -29,44 +68,59 @@ E_DragAndDropSwitcher* E_DragAndDropSwitcher::getSingleton()
 
 void E_DragAndDropSwitcher::startSwitcher()
 {
-	E_Global* e_global = E_Global::getSingleton();
 	E_EnvironmentManager* enManager = E_EnvironmentManager::getSingleton();
-	CBrush m_oBkgndBrush;
-	m_oBkgndBrush.CreateSolidBrush(RGB(255, 255, 255));
-	UINT nClassStyle = CS_NOCLOSE | CS_VREDRAW | CS_HREDRAW | CS_DBLCLKS;
-	CString szClassName = AfxRegisterWndClass(nClassStyle, 0, (HBRUSH)m_oBkgndBrush.GetSafeHandle(), 0);
 
-	std::list<E_Desktop*> desktopList = e_global->desktopList;
-	int check = 0;
-	for (std::list<E_Desktop*>::iterator itr_Desktop = desktopList.begin(); itr_Desktop != desktopList.end(); itr_Desktop++)
+	/*if (!ison)
 	{
-		if (check)
+		E_Global* e_global = E_Global::getSingleton();
+
+		CBrush brush_window;
+		UINT nClassStyle_window = 0;
+		CString szClassName_window = AfxRegisterWndClass(nClassStyle_window, 0, (HBRUSH)CreateSolidBrush(Color::Black), 0);
+		if (cursour_left)
 		{
-			nextDesktopIndex = (*itr_Desktop)->getIndex();		//맨 끝인경우는?? 환형리스트필요?
-			break;
+			CreateEx(WS_EX_TOPMOST | WS_EX_TOOLWINDOW, szClassName_window, L"DragAndDropSwitcher", WS_VISIBLE | WS_POPUP, CRect(-(enManager->getWidth() * 2) + 5, 0, 0 + 5, enManager->getHeight()), CWnd::GetDesktopWindow(), 0);
 		}
-		if (e_global->getSelectedIndex() == (*itr_Desktop)->getIndex())
-			check = 1;			
-		Create(szClassName, _T(""), WS_VISIBLE, CRect(enManager->getWidth()-100, 0, enManager->getWidth()*2-10, enManager->getHeight()), CWnd::GetDesktopWindow(), 1234); // nid 뭔지 꼭 찾기.
+		else if (cursour_right)
+		{
+			CreateEx(WS_EX_TOPMOST | WS_EX_TOOLWINDOW, szClassName_window, L"DragAndDropSwitcher", WS_VISIBLE | WS_POPUP, CRect(enManager->getWidth() - 5, 0, enManager->getWidth() * 3 - 5, enManager->getHeight()), CWnd::GetDesktopWindow(), 0);
+		}
+		else if (cursour_top)
+		{
+			CreateEx(WS_EX_TOPMOST | WS_EX_TOOLWINDOW, szClassName_window, L"DragAndDropSwitcher", WS_VISIBLE | WS_POPUP, CRect(0, -(enManager->getHeight() * 2) + 5, enManager->getWidth(), 0 + 5), CWnd::GetDesktopWindow(), 0);
+		}
+		else if (cursour_bottom)
+		{
+			CreateEx(WS_EX_TOPMOST | WS_EX_TOOLWINDOW, szClassName_window, L"DragAndDropSwitcher", WS_VISIBLE | WS_POPUP, CRect(0, enManager->getHeight() - 5, enManager->getWidth(), enManager->getHeight() * 3 - 5), CWnd::GetDesktopWindow(), 0);
+		}
+		
+		E_Window::setIconInvisible(this->m_hWnd);
 
+		drawDragAndDropSwitcher();
+		UpdateWindow();
+		ison = true;
+
+		ShowWindow(SW_SHOWMAXIMIZED);
+
+		//e_global->startUpdate();
 	}
-	
-
-
-
-
-
-
-
-
-
-
-
+	else
+	{
+		terminateSwitcher();
+	}*/
 }
 
 void E_DragAndDropSwitcher::terminateSwitcher()
 {
+	E_Global* e_global = E_Global::getSingleton();
+//	eraseWindowAnimation();
 
+	ison = false;
+
+	//e_global->stopUpdate();
+
+	E_Window::setIconVisible(this->m_hWnd);
+	DestroyWindow();
 }
 
 void E_DragAndDropSwitcher::OnResize()
